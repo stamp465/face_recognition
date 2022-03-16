d7cf3c4bc057c922f337582640c94ad1d1ed0aa8
tawanchaiii <tawanchai_champ@hotmail.com>
2022-03-16 14:59:12 +0700

    finish

--- blink/blink.ino
+++ blink/blink.ino
@@ -3,7 +3,7 @@
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo myservo;
 #define pass_length 10
 
@@ -13,10 +13,11 @@ Servo myservo;
 #define RQ_RESULT 3
 #define RQ_GET_START_STATUS 4
 
-#define button_sw !(digitalRead(PIN_PB0)&&digitalRead(PIN_PB1)&&digitalRead(PIN_PB2)&&digitalRead(PIN_PB3)&&digitalRead(PIN_PB4)&&digitalRead(PIN_PB5))
+#define button_sw !(digitalRead(PIN_PB0) && digitalRead(PIN_PB1) && digitalRead(PIN_PB2) && digitalRead(PIN_PB3) && digitalRead(PIN_PB4) && digitalRead(PIN_PB5))
 
 uint8_t password[pass_length];
 uint8_t password_notsubmit[pass_length];
+int print_waiting = 0;
 int size_now = 0;
 int set_start = 0;
 int old_result = 0;
@@ -27,55 +28,64 @@ bool waiting = false;
 static uint8_t pw = 0;
 static uint8_t n_pw = 0;
 
-unsigned long savetime;
+unsigned long savetime, time_w;
 
-
-void print_start(){
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("ENTER YOUR FACE");
-}
-
-void print_correct(){
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Correct");
-}
-
-void print_door(bool status){
-  lcd.clear();
-  lcd.setCursor(0,1);
-  if(status){
-    lcd.print("Open Door");
-    /*
-    servo ....
-    */
-  }
-  else if(!status){
-    lcd.print("Close Door");
-    /*
-    servo ....
-    */
-  }
-    
-}
-
-void print_password(){
+void print_start()
+{
     lcd.clear();
-    lcd.setCursor(0,0);
-    if(waiting)     lcd.print("Waiting Result");
-    else{
-        if(!submit){
-            if(old_result==1)   lcd.print("Wrong, ");
+    lcd.setCursor(0, 0);
+    lcd.print("ENTER YOUR FACE");
+}
+
+void print_correct()
+{
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Correct");
+}
+
+void print_door(bool status)
+{
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    if (status)
+    {
+        lcd.print("Open Door");
+        myservo.write(90);
+    }
+    else if (!status)
+    {
+        lcd.print("Close Door");
+        myservo.write(0);
+    }
+}
+
+void print_password()
+{
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    if (waiting)
+    {
+        lcd.print("Waiting Result");
+    }
+    else
+    {
+        if (!submit)
+        {
+            if (old_result == 1)
+                lcd.print("Wrong, ");
             lcd.print("Password :");
         }
-        else    lcd.print(" Password Sent -->");
+        else
+            lcd.print(" Password Sent -->");
     }
-    
+
     int i;
-    for(i=0;i<size_now;i++){
-        lcd.setCursor(i,1);
-        if(password[i]!=0){
+    for (i = 0; i < size_now; i++)
+    {
+        lcd.setCursor(i, 1);
+        if (password[i] != 0)
+        {
             lcd.print(password[i]);
         }
     }
@@ -86,57 +96,71 @@ usbMsgLen_t usbFunctionSetup(uint8_t data[8])
     usbRequest_t *rq = (usbRequest_t *)data;
     static uint8_t switch_state[6];
 
-    if (rq->bRequest == RQ_SET_LED){
+    if (rq->bRequest == RQ_SET_LED)
+    {
         uint8_t led_val = rq->wValue.bytes[0];
         uint8_t led_no = rq->wIndex.bytes[0];
-        //if (led_no == 0)    digitalWrite(PIN_PC2, led_val);
-        return 0; 
+        // if (led_no == 0)    digitalWrite(PIN_PC2, led_val);
+        return 0;
     }
-    else if (rq->bRequest == RQ_SET_START){
+    else if (rq->bRequest == RQ_SET_START)
+    {
         uint8_t if_face = rq->wValue.bytes[0];
-        if (if_face == 1 && set_start == 0 ){                // when see face and before status is close door
-            set_start = 1; 
+        if (if_face == 1 && set_start == 0)
+        { // when see face and before status is close door
+            set_start = 1;
             state = 1;
         }
         /*else if (if_face == 0 && set_start != 2){            // when not see face and status is not open door
-            set_start = 0; 
+            set_start = 0;
             state = 0;
         }*/
-        return 0; 
+        return 0;
     }
-    else if (rq->bRequest == RQ_GET_PW){
+    else if (rq->bRequest == RQ_GET_PW)
+    {
 
         // check all is zero ?
         int i;
         bool allnotZERO = false;
-        for(i=0;i<pass_length;i++)  if(password[i]!=0)  allnotZERO = true;
+        for (i = 0; i < pass_length; i++)
+            if (password[i] != 0)
+                allnotZERO = true;
 
-        if(submit && !waiting && allnotZERO){       // when submit and not waiting for result and all is not zero
-            usbMsgPtr = (uint8_t*) &password;
+        if (submit && !waiting && allnotZERO)
+        { // when submit and not waiting for result and all is not zero
+            usbMsgPtr = (uint8_t *)&password;
             waiting = true;
+            time_w = millis();
             print_password();
         }
-        else if(submit && !allnotZERO){             // when submit and all is zero --- ignore waiting
+        else if (submit && !allnotZERO)
+        { // when submit and all is zero --- ignore waiting
             submit = false;
-            usbMsgPtr = (uint8_t*) &password_notsubmit;
+            usbMsgPtr = (uint8_t *)&password_notsubmit;
             print_password();
         }
-        else{                                       // when not submit ( python call all time )
-            usbMsgPtr = (uint8_t*) &password_notsubmit;
+        else
+        { // when not submit ( python call all time )
+            usbMsgPtr = (uint8_t *)&password_notsubmit;
         }
         return sizeof(password);
     }
-    else if (rq->bRequest == RQ_RESULT){
-        if(submit){
+    else if (rq->bRequest == RQ_RESULT)
+    {
+        if (submit)
+        {
             submit = false;
             waiting = false;
             uint8_t open_door = rq->wValue.bytes[0];
-            if(open_door==0){               // wrong password
+            if (open_door == 0)
+            { // wrong password
                 door = false;
                 old_result = 1;
                 print_password();
             }
-            else if(open_door==1){          // correct password
+            else if (open_door == 1)
+            { // correct password
                 door = true;
                 set_start = 2;
                 state = 2;
@@ -145,18 +169,21 @@ usbMsgLen_t usbFunctionSetup(uint8_t data[8])
         }
         return 0;
     }
-    else if (rq->bRequest == RQ_GET_START_STATUS){
-        usbMsgPtr = (uint8_t*) &set_start;
+    else if (rq->bRequest == RQ_GET_START_STATUS)
+    {
+        usbMsgPtr = (uint8_t *)&set_start;
         return 1;
     }
     return 0;
 }
 
-void reset_password(){
+void reset_password()
+{
     int i;
-    for(i=0;i<pass_length;i++){
-      password_notsubmit[i]=0;
-      password[i]=0;
+    for (i = 0; i < pass_length; i++)
+    {
+        password_notsubmit[i] = 0;
+        password[i] = 0;
     }
     size_now = 0;
 }
@@ -164,21 +191,21 @@ void reset_password(){
 void setup()
 {
 
-    pinMode(PIN_PC0,OUTPUT);
-    pinMode(PIN_PC1,OUTPUT);
-    pinMode(PIN_PC2,OUTPUT);
+    pinMode(PIN_PC0, OUTPUT);
+    pinMode(PIN_PC1, OUTPUT);
+    pinMode(PIN_PC2, OUTPUT);
 
-    pinMode(PIN_PB0,INPUT_PULLUP);
-    pinMode(PIN_PB1,INPUT_PULLUP);
-    pinMode(PIN_PB2,INPUT_PULLUP);
-    pinMode(PIN_PB3,INPUT_PULLUP);
-    pinMode(PIN_PB4,INPUT_PULLUP);
-    pinMode(PIN_PB5,INPUT_PULLUP);
+    pinMode(PIN_PB0, INPUT_PULLUP);
+    pinMode(PIN_PB1, INPUT_PULLUP);
+    pinMode(PIN_PB2, INPUT_PULLUP);
+    pinMode(PIN_PB3, INPUT_PULLUP);
+    pinMode(PIN_PB4, INPUT_PULLUP);
+    pinMode(PIN_PB5, INPUT_PULLUP);
     myservo.attach(PIN_PD6);
-    //myservo.write(0);
+    myservo.write(0);
 
-    lcd.init();  //initialize the lcd
-    lcd.backlight();  //open the backlight 
+    lcd.init();      // initialize the lcd
+    lcd.backlight(); // open the backlight
     print_start();
     delay(1000);
 
@@ -190,92 +217,122 @@ void setup()
     usbDeviceDisconnect();
     delay(300);
     usbDeviceConnect();
-
-    
-
 }
 void loop()
 {
     usbPoll();
 
-    if(set_start==1){
-        if(state==1){
+    if (set_start == 1)
+    {
+        if (state == 1)
+        {
             print_password();
             state = 0;
         }
-        usbPoll();
-        if(button_sw){
-            if(!digitalRead(PIN_PB0) && size_now < pass_length ){           //if 3 pressed and password not full of screen
-                password[size_now] = 3;
-                size_now ++;
-                while(!digitalRead(PIN_PB0))
-                  usbPoll();
+        if (millis() - time_w > 3500 && waiting)
+        {
+            if(!print_waiting){
+                print_waiting = 1;
+                lcd.clear();
+                lcd.setCursor(0, 0);
+                lcd.print("Sent ERROR");
+                lcd.setCursor(0, 1);
+                lcd.print("Please sent again");
+                time_w = millis();
             }
-            else if(!digitalRead(PIN_PB1) && size_now < pass_length ){      //if 4 pressed and password not full of screen
-                password[size_now] = 5;
-                size_now ++;
-                while(!digitalRead(PIN_PB1))
-                  usbPoll();
-            }
-            else if(!digitalRead(PIN_PB2) && size_now < pass_length ){      //if 4 pressed and password not full of screen
-                password[size_now] = 7;
-                size_now ++;
-                while(!digitalRead(PIN_PB2))
-                  usbPoll();
-            }
-            else if(!digitalRead(PIN_PB3) && size_now < pass_length ){      //if 4 pressed and password not full of screen
-                password[size_now] = 9;
-                size_now ++;
-                while(!digitalRead(PIN_PB3))
-                  usbPoll();
-            }
+           
+            if (millis() - time_w > 1500)
+            {
+                submit = false;
+                waiting = false;
+                print_password();
+                print_waiting = 0;
+                old_result = 0;
             
-            else if(!digitalRead(PIN_PB5) && size_now > 0){                 //backspace if have password in screen
-                password[size_now-1] = 0;
-                size_now --;
-                while(!digitalRead(PIN_PB5))
-                  usbPoll();
             }
-            else if(!digitalRead(PIN_PB4) ){                                //if ENTER
-                submit=true;
-                while(!digitalRead(PIN_PB4))
-                  usbPoll();
+        }
+        usbPoll();
+        if (button_sw)
+        {
+            if (!digitalRead(PIN_PB0) && size_now < pass_length)
+            { // if 3 pressed and password not full of screen
+                password[size_now] = 3;
+                size_now++;
+                while (!digitalRead(PIN_PB0))
+                    usbPoll();
+            }
+            else if (!digitalRead(PIN_PB1) && size_now < pass_length)
+            { // if 5 pressed and password not full of screen
+                password[size_now] = 5;
+                size_now++;
+                while (!digitalRead(PIN_PB1))
+                    usbPoll();
+            }
+            else if (!digitalRead(PIN_PB2) && size_now < pass_length)
+            { // if 7 pressed and password not full of screen
+                password[size_now] = 7;
+                size_now++;
+                while (!digitalRead(PIN_PB2))
+                    usbPoll();
+            }
+            else if (!digitalRead(PIN_PB3) && size_now < pass_length)
+            { // if 9 pressed and password not full of screen
+                password[size_now] = 9;
+                size_now++;
+                while (!digitalRead(PIN_PB3))
+                    usbPoll();
+            }
+
+            else if (!digitalRead(PIN_PB5) && size_now > 0)
+            { // backspace if have password in screen
+                password[size_now - 1] = 0;
+                size_now--;
+                while (!digitalRead(PIN_PB5))
+                    usbPoll();
+            }
+            else if (!digitalRead(PIN_PB4))
+            { // if ENTER
+                submit = true;
+                while (!digitalRead(PIN_PB4))
+                    usbPoll();
             }
             print_password();
         }
-
-        
-
     }
-    else if(set_start==2){
-        if(state == 2){
+    else if (set_start == 2)
+    {
+        if (state == 2)
+        {
             print_correct();
             savetime = millis();
             state = 22;
         }
-        else if(state==22 && millis() - savetime > 1000){
+        else if (state == 22 && millis() - savetime > 1000)
+        {
             print_door(door);
             state = 222;
         }
-        else if(state==222 && millis() - savetime > 8000){
+        else if (state == 222 && millis() - savetime > 8000)
+        {
             door = false;
             print_door(door);
             state = -1;
         }
-        else if( millis() - savetime > 11000){
+        else if (millis() - savetime > 11000)
+        {
             state = 0;
             set_start = 0;
             reset_password();
         }
     }
-    else{
-        
-        if(state == 0){
+    else
+    {
+
+        if (state == 0)
+        {
             print_start();
             reset_password();
             state = -1;
         }
-        
     }
-
 }
--- examples/facerec_from_webcam_faster.py
+++ examples/facerec_from_webcam_faster.py
@@ -8,15 +8,6 @@ devices = find_mcu_boards()
 mcu = McuBoard(devices[0])
 peri = PeriBoard(mcu)
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
@@ -55,61 +46,22 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
+timeout = 150           # 150 sec
+start_time = 0
+
+
 while True:
 
-    start_tt = mcu.usb_read(4, length=1)
-    if start_tt[0] == 2 :
-        print("open door")
-        time.sleep(1)
-        continue
+    try :
+        start_tt = mcu.usb_read(4, length=1)
 
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+        # 2 == door opened
+        if start_tt[0] == 2 :
+            print("open door")
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
-
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
-            # peri.set_led(1,0)
-
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-
-
-
-            face_names.append(name)
-        if("Champ" in face_names):
-            # mcu.usb_write(0, value=1, index=0)
-            # mcu.usb_write(3, value=90)
-            # peri.set_led(1,1)
-
-            mcu.usb_write(1, value=1)         # set start
-            timeout = 150           # 150 sec
-            start_time = time.time()
-            
-            
-            while time.time() < start_time + timeout:
+        # 1 == check pass
+        if start_tt[0] == 1 :
+            if time.time() < start_time + timeout:
                 tmp = mcu.usb_read(2, length=10)
                 
                 read_hardware_password = ""
@@ -123,51 +75,67 @@ while True:
                     pass
                 elif read_hardware_password == "3579" :
                     print("Is TRUE")
-                    mcu.usb_write(3, value=1) 
-                    break
+                    mcu.usb_write(3, value=1)
+                    # break
                 else :
                     print("Is False")
-                    mcu.usb_write(3, value=0) 
+                    mcu.usb_write(3, value=0)
+
+            else:
+                mcu.usb_write(1, value=0)         # set end        
+
+        # 0 == check face
+        if start_tt[0] == 0 :
+            # Grab a single frame of video
+            ret, frame = video_capture.read()
+
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            # Only process every other frame of video to save time
+            if process_this_frame:
+                # Find all the faces and face encodings in the current frame of video
+                face_locations = face_recognition.face_locations(rgb_small_frame)
+                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+                face_names = []
+                for face_encoding in face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                    name = "Unknown"
+
+                    # Or instead, use the known face with the smallest distance to the new face
+                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+                    face_names.append(name)
                 
-                time.sleep(1)       # sleep 5 sec
-            
-            mcu.usb_write(1, value=0)         # set end
-            
+                if("Champ" in face_names):
+                    # mcu.usb_write(0, value=1, index=0)
+                    # mcu.usb_write(3, value=90)
+                    # peri.set_led(1,1)
 
-        #else:
-            # mcu.usb_write(0, value=0, index=0)
-            # mcu.usb_write(3, value=0)
-            # peri.set_led(1,0)
-        # if("Stamp" in face_names):
-        #     peri.set_led(0,1)
-        # else:
-        #     peri.set_led(0,0)
-        print(face_names)
-    process_this_frame = not process_this_frame
+                    mcu.usb_write(1, value=1)         # set start
+                    
+                    start_time = time.time()
+                    
+                print(face_names)
+            process_this_frame = not process_this_frame
 
+    except :
+        print("ERROR !!!!!!!!!!!!!!!!!!!!!!")
+        devices = find_mcu_boards()
+        mcu = McuBoard(devices[0])
+        peri = PeriBoard(mcu)
+    
+    time.sleep(1)       # sleep 1 sec
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+    
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
 # Release handle to the webcam
 video_capture.release()
